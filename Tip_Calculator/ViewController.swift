526c82c77546cc9a2c5ddff72973c91c0b1f1cf1
Dan <dmorton2297@gmail.com>
2015-07-03 14:00:31 -0500

    This is an initial commit

--- Tip_Calculator/ViewController.swift
+++ Tip_Calculator/ViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func percentButtonClicked(sender: AnyObject) {
+        amountTextField.resignFirstResponder()
+        var billAmount : Double!
+        if let userInput : String = amountTextField.text {
+            billAmount = Double(userInput)
+        }
+        
+        if billAmount == nil {
+            totalAmountLabel.text = "$0.00"
+            tipAmountLabel.text = "$0.00"
+            return
+        }
+        
+        
+        var tipAmount : Double!
+        switch sender.tag{
+        case 10 :
+            tipAmount = billAmount * 0.10
+        case 15 :
+            tipAmount = billAmount * 0.15
+        case 20 :
+            tipAmount = billAmount * 0.20
+        case 25 :
+            tipAmount = billAmount * 0.25
+        default:
+            print("Defualt Case")
+        }
+        
+        tipAmount = round(tipAmount * 100) / 100
+        let totalAmount = billAmount + tipAmount
+        
+        tipAmountLabel.text = "$\(tipAmount)"
+        totalAmountLabel.text = "$\(totalAmount)"
+        
+        self.view.endEditing(true)
     }
-
-
 }
 
